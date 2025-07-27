f76fa58ba3d54b868dc00d03b63f5c18910ffe21
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-07-27 21:33:00 +0100

    added button functionality

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,12 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -40,13 +46,11 @@ class MyHomePage extends StatelessWidget {
         // ↓ Add this.
           ElevatedButton(
             onPressed: () {
-              print('button pressed!');
+              appState.getNext(); 
             },
             child: Text('Next'),
           ),
         ],
-        
-
         
       ),
     );
