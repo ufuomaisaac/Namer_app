bf6049c202be668603e829e105ba06e8f88b7473
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-07-27 19:33:32 +0100

    added a button to the application

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,19 @@ class MyHomePage extends StatelessWidget {
 
     return Scaffold(
       body: Column(
-        children: [Text('A random idea:'), Text(appState.current.asLowerCase)],
+        children: [Text('A random idea:'),
+        Text(appState.current.asLowerCase),
+        // ↓ Add this.
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+            },
+            child: Text('Next'),
+          ),
+        ],
+        
+
+        
       ),
     );
   }
